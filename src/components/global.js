d7a3700e06d6a0968dd34b6e6ddc2cc17b271775
Jon Ambas <jon@jonambas.com>
2020-04-07 20:59:32 -0400

    Try againt to fix fouc

---
+++ src/components/global.js
@@ -0,0 +1,81 @@
+export const global = `
+  html {
+    font-family: sans-serif;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, \'San Francisco\', \'Segoe UI\', Roboto, Helvetica, sans-serif;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body {
+    min-height: 100%;
+  }
+  * {
+    box-sizing: inherit;
+  }
+  *:before {
+    box-sizing: inherit;
+  }
+  *:after {
+    box-sizing: inherit;
+  }
+  body {
+    color: hsla(0, 0%, 0%, 0.8);
+    font-family: -apple-system, BlinkMacSystemFont, \'San Francisco\', \'Segoe UI\', Roboto, Helvetica, sans-serif;
+  }
+  a,
+  a:visited {
+    color: #2c44f1;
+    transition: 0.15s;
+  }
+  a:hover,
+  a:visited:hover {
+    color: #f12c2c;
+  }
+  ::selection {
+    background: #f12c2c;
+    color: #fefefe;
+  }
+  p {
+    margin: 0;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  ul,
+  li {
+    margin: 0;
+  }
+  ul {
+    padding-left: 0;
+    list-style-type: none;
+  }
+  li {
+    position: relative;
+    padding-left: 1.25rem;
+  }
+
+  li:before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 9px;
+    width: 3px;
+    height: 3px;
+    border-radius: 50%;
+    background: black;
+  }
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,65 +9,70 @@ import React from "react"
 import PropTypes from "prop-types"
 import { ThemeProvider, Box } from "@sparkpost/matchbox"
 import ExternalLink from "./ExternalLink"
-import styled from "styled-components"
-import "./layout.css"
+import { createGlobalStyle } from "styled-components"
+import { global } from "./global"
 
-const StyledContent = styled("div")`
-  opacity: 1 !important;
+const GlobalStyle = createGlobalStyle`
+  ${global}
 `
 
 const Layout = ({ centered, children }) => {
   return (
-    <ThemeProvider>
-      <StyledContent className="content">
-        <main>
-          <Box
-            mx="600"
-            display={centered ? "flex" : ""}
-            height={centered ? "80vh" : ""}
-            alignItems={centered ? "center" : ""}
-            maxWidth={centered ? "" : "600px"}
-            py={centered ? "0" : "600"}
-          >
-            {children}
-          </Box>
-        </main>
-        <Box
-          as="footer"
-          position={centered ? "absolute" : ""}
-          bottom={centered ? "0" : ""}
-          display="flex"
-          flexWrap="wrap"
-          my="500"
-          ml="600"
-        >
-          <Box mr="700" mb="300" fontSize="100" fontWeight="500">
-            <ExternalLink to="https://github.com/jonambas">Github</ExternalLink>
-          </Box>
-
-          <Box mr="700" mb="300" fontSize="100" fontWeight="500">
-            <ExternalLink to="https://dribbble.com/jonambas">
-              Dribbble
-            </ExternalLink>
-          </Box>
-
-          <Box mr="700" mb="300" fontSize="100" fontWeight="500">
-            <ExternalLink to="https://twitter.com/jonambas">
-              @jonambas
-            </ExternalLink>
-          </Box>
-
-          <Box mr="700" mb="300" fontSize="100" fontWeight="500">
-            <ExternalLink
-              to="mailto:jon@jonambas.com?subject=Hello!"
-              title="Email me"
+    <>
+      <GlobalStyle />
+      <ThemeProvider>
+        <div>
+          <main>
+            <Box
+              mx="600"
+              display={centered ? "flex" : ""}
+              height={centered ? "80vh" : ""}
+              alignItems={centered ? "center" : ""}
+              maxWidth={centered ? "" : "600px"}
+              py={centered ? "0" : "600"}
             >
-              jon@jonambas.com
-            </ExternalLink>
+              {children}
+            </Box>
+          </main>
+          <Box
+            as="footer"
+            position={centered ? "absolute" : ""}
+            bottom={centered ? "0" : ""}
+            display="flex"
+            flexWrap="wrap"
+            my="500"
+            ml="600"
+          >
+            <Box mr="700" mb="300" fontSize="100" fontWeight="500">
+              <ExternalLink to="https://github.com/jonambas">
+                Github
+              </ExternalLink>
+            </Box>
+
+            <Box mr="700" mb="300" fontSize="100" fontWeight="500">
+              <ExternalLink to="https://dribbble.com/jonambas">
+                Dribbble
+              </ExternalLink>
+            </Box>
+
+            <Box mr="700" mb="300" fontSize="100" fontWeight="500">
+              <ExternalLink to="https://twitter.com/jonambas">
+                @jonambas
+              </ExternalLink>
+            </Box>
+
+            <Box mr="700" mb="300" fontSize="100" fontWeight="500">
+              <ExternalLink
+                to="mailto:jon@jonambas.com?subject=Hello!"
+                title="Email me"
+              >
+                jon@jonambas.com
+              </ExternalLink>
+            </Box>
           </Box>
-        </Box>
-      </StyledContent>
-    </ThemeProvider>
+        </div>
+      </ThemeProvider>
+    </>
   )
 }
 
